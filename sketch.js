07760c833e619b8588064f2c70b8b94d14426d7e
PixelRifts <njk777400@gmail.com>
2021-03-21 19:00:31 +0530

    A bit mmore randomisation

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function draw() {
 
     for (let i = 0; i < objects.length; i++) {
         objects[i].update();
-	for (let j = 0; j < objects.length; j++) {
+	    for (let j = 0; j < objects.length; j++) {
             if (objects[i] != objects[j]) {
                 objects[i].attract(objects[j]);
             }
@@ -22,6 +22,11 @@ function draw() {
 
 function mouseClicked() {
     let p = color(50, 255, 50);
-    objects.push(new CelestialObject(mouseX, mouseY, 10, p5.Vector.random2D().setMag(5), p));
+    let vec = p5.Vector.sub(createVector(mouseX, mouseY), objects[0].pos).setMag(5);
+    let lr = random(0, 100);
+    if (lr <= 50)
+        objects.push(new CelestialObject(mouseX, mouseY, 10, createVector(vec.y, -vec.x), p));
+    else 
+        objects.push(new CelestialObject(mouseX, mouseY, 10, createVector(-vec.y, vec.x), p));
 }
 
