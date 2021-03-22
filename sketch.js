5158a8bcca9881aa8bc23dbe2bbbde16cb1f7d95
PixelRifts <njk777400@gmail.com>
2021-03-22 13:05:37 +0530

    Updates

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ let objects = [];
 function setup() {
     createCanvas(1200, 800);
     let s = color(255, 255, 50);
-    objects[0] = new CelestialObject(600, 400, 500, createVector(0, 0), s);
 }
 
 function draw() {
@@ -21,13 +20,10 @@ function draw() {
 }
 
 function mouseClicked() {
-    let vec = p5.Vector.sub(createVector(mouseX, mouseY), objects[0].pos).setMag(5);
+    let vec = createVector();
     let lr = random(0, 100);
     let mass = random(5, 40);
     let c = color(random(255), random(255), random(255));
-    if (lr <= 50)
-        objects.push(new CelestialObject(mouseX, mouseY, mass, createVector(vec.y, -vec.x), c));
-    else 
-        objects.push(new CelestialObject(mouseX, mouseY, mass, createVector(-vec.y, vec.x), c));
+    if (lr <= 50) objects.push(new CelestialObject(mouseX, mouseY, mass, createVector(vec.y, -vec.x), c));
+    else objects.push(new CelestialObject(mouseX, mouseY, mass, createVector(-vec.y, vec.x), c));
 }
-
